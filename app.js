7344f2e5290c5120218e395cbd834bb88ef73ef1
prathambhatia86 <85384335+prathambhatia86@users.noreply.github.com>
2023-12-23 21:37:11 +0530

    frontend added and initializing backend

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const express=require('express')
 const app=express()
+const path = require('path');
+const cors = require('cors');
+app.use(express.json({ limit: "50mb" }))
+app.use(cors());
+app.use(express.urlencoded({ extended: false, limit: "50mb" }))
+app.use(express.static(path.join(__dirname, 'frontend.html')))
