b7511f58c4f0835519d91a8aaca8f73e244317c4
prathambhatia86 <85384335+prathambhatia86@users.noreply.github.com>
2023-12-27 01:43:32 +0530

    integrated react with backend and added add random entries functionality on frontend

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ const randomstring = require("randomstring");
 app.use(express.json({ limit: "50mb" }))
 app.use(cors());
 app.use(express.urlencoded({ extended: false, limit: "50mb" }))
-app.use(express.static(path.join(__dirname, 'frontend.html')))
-const pincodes=[];
+// app.use(express.static(path.join(__dirname, 'frontend.html')))
+const pincodes=new Set();
 const names=[];
 const mapping = new Map();
+
 create_random_pincodes=()=>{
     for(let i=0;i<30000;i++)
     {
     const randomSixDigitNumber = Math.floor(100000 + Math.random() * 900000);
-    pincodes.push(randomSixDigitNumber);
+    a=randomSixDigitNumber;
+    pincodes.add(randomSixDigitNumber);
     }
 };
 create_random_strings=()=>{
@@ -28,6 +30,31 @@ create_random_strings=()=>{
       mapping.set(name,pincodes);
     }
 }
-create_random_pincodes();
-create_random_strings();
-console.log(mapping.size)
+
+app.post("/addRandomEntries",async(req,res)=>{
+    create_random_pincodes();
+    create_random_strings();
+    console.log(names.length);
+    res.send(true);
+})
+
+// check_entries=()=>{
+// if(mapping.has(names[1000000]))
+// {
+//     let temp=new Set();
+//     temp=mapping.get(names[100000]);
+//     if(temp.has(a))
+//     console.log("the pin facility is available with the particular merchant");
+//     else
+//     console.log("the merchant dont deliver to this location");
+// }
+// else
+// {
+//     console.log("this merchant does not exist or has not enabled pincode facility");
+// }
+// };
+
+// check_entries();
+app.listen(8000,()=>{
+    console.log("conection is successful");
+})
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,30 @@
-import React from 'react'
-export default function home()
+import React,{useState} from 'react'
+import axios from 'axios';
+import RingLoader from "react-spinners/RingLoader";
+export default function Home()
 {
-    
+  const [showEntriesDialog, setShowEntriesDialog] = useState(false);
+    const addRandomEntries=async ()=>{
+      setShowEntriesDialog(true);
+      console.log("yoo");
+      let result=await axios.post("http://localhost:8000/addRandomEntries");
+      console.log(result);
+      if(result)
+      setShowEntriesDialog(false);
+    }
     return(
-        
-         <div className="container mt-5">
+      <div className="container mt-5">
+         {showEntriesDialog &&(
+            <RingLoader
+            color={"#000066"}
+            loading={showEntriesDialog}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />)}
+       
+      
       <h2>sparse matrix</h2>
-  
-      <form>
         <div className="form-group">
           <label for="pincode">Pincode:</label>
           <input type="text" className="form-control" id="pincode" placeholder="Enter Pincode" />
@@ -18,9 +35,8 @@ export default function home()
           <input type="text" className="form-control" id="merchantName" placeholder="Enter Merchant Name" />
         </div>
   
-        <button type="button" className="btn btn-primary" >Check</button>
-        <button type="button" className="btn btn-secondary">Add Random Entries</button>
-      </form>
+        <button type="submit" className="btn btn-primary"  >Check</button>
+        <button type="submit" className="btn btn-secondary" onClick={addRandomEntries}>Add Random Entries</button>
     </div>
         
     )
