ea352e12d91d5773375f8524cdadb44302008ead
prathambhatia86 <85384335+prathambhatia86@users.noreply.github.com>
2024-02-05 19:32:43 +0530

    todo of username completed

--- frontend/src/components/MerchantHome.js
+++ frontend/src/components/MerchantHome.js
@@ -1,11 +1,12 @@
-import { React, useState } from "react"
+import { React, useEffect, useState } from "react"
 import AddPincode from "./AddPincode"
 import { CgProfile } from "react-icons/cg";
 import { TbHomeEdit } from "react-icons/tb";
-
+import { jwtDecode } from "jwt-decode";
 export default function MerchantHome() {
     const [isClickedEditPincode, toggleEditPincodes] = useState(false);
     const [isClickedProfile, toggleCheckProfile] = useState(false);
+    const [userName,changeUsername]=useState("");
     const editPincodes = () => {
         toggleEditPincodes(prev => !(prev));
         toggleCheckProfile(false);
@@ -14,9 +15,14 @@ export default function MerchantHome() {
         toggleCheckProfile(prev => !(prev));
         toggleEditPincodes(false);
     }
+    useEffect(()=>{
+        const token=sessionStorage.getItem("token");
+        const decoded = jwtDecode(token);
+        changeUsername(decoded.username);
+    },[]);
     return (
         <div class="container mt-5">
-            <h2 class="text-center mb-4">Welcome [TODO Add UserName]</h2>
+            <h2 class="text-center mb-4">Welcome {userName}</h2>
 
             <div class="row justify-content-center">
                 <div class="col-md-6">
